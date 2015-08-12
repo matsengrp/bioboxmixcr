d4897ae7e5db75e6dd98724013f6aa7fec9f3cb9
Andrew Wei <andrewwei98@gmail.com>
2015-08-12 13:22:47 -0700

    minor fixes

--- python/mixcrparser.py
+++ python/mixcrparser.py
@@ -3,7 +3,6 @@
 from performanceplotter import PerformancePlotter
 import csv
 import utils
-import fillData
 #----------------------------
 #hardcoded default germline sequences
 germline_seqs = utils.read_germlines("data/imgt") 
@@ -11,7 +10,7 @@ germline_seqs = utils.read_germlines("data/imgt")
 #create an instance of the performance plotter class
 perfplotter = PerformancePlotter(germline_seqs, 'mixcr')
 
-#fill in both inferred and true dictionaries
+#fill in both inferred and true nested dictionaries
 trueDictionary = {}
 iDictionary = {}
 with open("simu-10-leaves-1-mutate.csv") as inFile1:
@@ -23,21 +22,22 @@ with open("simu-10-leaves-1-mutate.csv") as inFile1:
 			trueDictionary[unique_id] = i1
 			iDictionary[unique_id] = i1
 			#print 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+			#print unique_id, '\n'
 			#print i1, '\n'
 			#print i2, '\n'
-			#print trueDictionary, '\n'
-			#print iDictionary, '\n'			
 			iDictionary[unique_id]['v_gene'] = i2['Best V hit']
 			iDictionary[unique_id]['d_gene'] = i2['Best D hit']
 			iDictionary[unique_id]['j_gene'] = i2['Best J hit']
-			#print iDictionary[unique_id]			
+			#print iDictionary[unique_id], '\n'			
 
 for tkey, ikey in zip(trueDictionary, iDictionary):
-	print tkey, unique_id
-	if trueDictionary[tkey][unique_id] == 123818946361786991: 
+	print tkey, ikey 
+	if tkey == 123818946361786991: 
 		utils.print_reco_event(germline_seqs, trueDictionary[tkey])
-	#print 'TRUE: ', trueDictionary[tkey], '\n', 'INFERRED: ', iDictionary[ikey], '\n'
 		perfplotter.evaluate(trueDictionary[tkey], iDictionary[ikey])
+	print 'XXXXXXXXXXXX'
+	#print 'TRUE: ', trueDictionary[tkey], '\n', 'INFERRED: ', iDictionary[ikey], '\n'
+	#perfplotter.evaluate(trueDictionary[tkey], iDictionary[ikey])
 
 #tkey = 123818946361786991
 #tempDict = trueDictionary['unique_id'][tkey]
