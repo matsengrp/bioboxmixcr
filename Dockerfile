c2e6932e386e6e8a25359c4b5733e84fa8a7a65e
Andrew Wei <andrewwei98@gmail.com>
2015-08-17 14:37:54 -0700

    currently working, does not include parameters yet

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,19 @@
 FROM matsengrp/cpp
-#RUN apt-get update
-#RUN apt-get install -y \	
-#	libroot-bindings-python-dev \	
-#	libroot-graf2d-postscript5.34 
+RUN sed 's/main$/main universe/' -i /etc/apt/sources.list
+RUN apt-get update && apt-get install -y software-properties-common python-software-properties
+RUN add-apt-repository ppa:webupd8team/java -y
+RUN apt-get update
+RUN echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+RUN apt-get install -y \
+    oracle-java7-installer \
+    libncurses5-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    r-base \
+    zlib1g-dev
 
 COPY . /mixcr
 WORKDIR /mixcr
-
-#RUN echo ${PWD}
-
 ADD run.sh /usr/local/bin/
-#RUN cd /usr/local/bin
-
-#RUN echo ${PWD}
-#RUN ls
-
+RUN cd /usr/local/bin && chmod 700 run.sh
 ENTRYPOINT ["./run.sh"]
--- run.sh
+++ run.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 #This script runs the biobox for mixcr. It is referenced as the entrypoint in the Dockerfile.
-#==============================================
-echo 'CHECK: exit scripts if cases fail'
+echo '#=============================================='
+echo 'CHECK(exit scripts if cases fail)'
 # exit script if one command fails
 set -o errexit
 # exit script if Variable is not set
 set -o nounset
-#==============================================
+echo '#=============================================='
 echo 'ASSIGNING BASIC VARIABLES'
-INPUT=/bbx/input/biobox.yml
-OUTPUT=/bbx/output
-#TASK=$1
-mkdir -p ${OUTPUT}
-#==============================================
+YAMLINPUT=/bbx/input/biobox.yml
+DOCKEROUTPUTDIR=/bbx/output
+mkdir -p ${DOCKEROUTPUTDIR}
+#ls ${DOCKEROUTPUTDIR}
+echo '#=============================================='
 echo 'CACHING PARAMETERS'
-#Parse the input yaml file and read the parameters
-#echo 'biobox.yml:'
-#cat ./inputDir/biobox.yml
 . ./inputDir/parse_yaml.sh
-eval $(parse_yaml $INPUT 'mixcr_')
+eval $(parse_yaml $YAMLINPUT 'mixcr_')
 
-GERMLINESEQDIR=$mixcr_germlineSeqDir
 INPUTFILE=$mixcr_inputfile
-OUTPUTFILE=$mixcr_outputfile
-
-#echo $GERMLINESEQDIR
 #echo $INPUTFILE
-#echo $OUTPUTFILE
-
+#ls 
+#ls inputDir
 #if the input file is in csv or tsv format run conversion scripts into fasta
 if [ ${INPUTFILE: -4} == ".csv" ] ; then
 	#run csv conversion script
@@ -43,38 +36,15 @@ elif [ ${INPUTFILE: -4} == ".tsv" ] ; then
 	python python/tsv2fasta.py $INPUTFILE
 	INPUTFILE=${INPUTFILE:0:${#INPUTFILE}-4}'.fasta'
 fi
-#echo $INPUTFILE
-#INPUTFILE=./inputDir/
-#ls inputDir
-#echo '==='
-#ls 
-#==============================================
-#echo 'GENERATING TASKFILE'
-#echo -n 'default: mixcr align ${INPUTFILE} output_file.vdjca && mixcr exportAlignments output_file.vdjca ${OUTPUTFILE}' >> ./Taskfile
-#ls
-#echo 'Taskfile: '
-#cat Taskfile
-#==============================================
-#CMD=$(egrep ^${TASK}: ./Taskfile | cut -f 2 -d ':')
-#if [[ -z ${CMD} ]]; then
-#	echo "Abort, no task found for '${TASK}'."
-#	exit 1
-#fi
-echo 'PROCESSING COMMAND'
-#echo $(pwd)
-ls
-ls outputDir
-#ls inputDir
-#CMD1='export PATH='$(pwd)':$PATH'
-#CMD2='mixcr align'$INPUTFILE'./outputDir/output_file.vdjca'
-#CMD3='mixcr exportAlignments ./outputDir/output_file.vdjca' $OUTPUTFILE
-echo $(pwd)
 echo $INPUTFILE
-echo $OUTPUTFILE
-CMD='export PATH='$(pwd)':$PATH && mixcr align'$INPUTFILE'./outputDir/output_file.vdjca && mixcr exportAlignments ./outputDir/output_file.vdjca'$OUTPUTFILE
-echo $CMD
+echo '#=============================================='
+echo 'PROCESSING COMMAND'
+CURRENTDIR=$(pwd)
+echo $CURRENTDIR
+echo $INPUTFILE
+CMD='export PATH=${CURRENTDIR}:$PATH && mixcr align ${INPUTFILE} output_file.vdjca && mixcr exportAlignments output_file.vdjca /bbx/output/output.txt'
 eval $CMD
-
+ls
 echo 'PROCESS COMPLETED'
 
 
