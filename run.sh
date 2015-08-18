1278a670e1f15ec46fb77f30cc49fb5cea8872cf
Andrew Wei <andrewwei98@gmail.com>
2015-08-18 14:32:01 -0700

    several minor tweaks

--- run.sh
+++ run.sh
@@ -16,6 +16,40 @@ echo '#=============================================='
 echo 'CACHING PARAMETERS'
 . ./inputDir/parse_yaml.sh
 eval $(parse_yaml $YAMLINPUT 'mixcr_')
+#create array to intake keys whose values are not false, add the values of those keys to a string 'CMD2'
+CMD2=''
+
+#currently checks one value at a time, future implementation will parse yaml file with a for loop
+#All_C_Alignments=$mixcr_ALLCAlignments
+#All_C_Hits=$mixcr_AllChits
+
+#All_D_Alignments=$mixcr_AllDalignments
+#All_D_Hits=$mixcr_AllDhits
+
+#All_V_Alignments=$mixcr_AllValignments
+#All_V_Alignments=$mixcr_AllValignments
+
+#All_J_Hits=$mixcr_AllJhits
+#All_J_Hits=$mixcr_AllJhits
+#===
+#Best_C_Alignment=$mixcr_BestCalignment
+#Best_C_Hit=$mixcr_BestChit
+
+#Best_D_Alignment=$mixcr_BestDalignment
+#Best_D_Hit=$mixcr_BestDhit
+
+#Best_V_Alignment=$mixcr_BestValignment
+#Best_V_Hit=$mixcr_BestVhit
+
+#Best_J_Alignment=$mixcr_BestJalignment
+#Best_J_Hit=$mixcr_BestJhit
+#===
+#DESCR1= 17 descrR1: false
+# 18 descrR2: false
+# 20 quality: false
+# 21 readId: false
+# 22 sequence: false
+# 23 targets: false
 
 INPUTFILE=$mixcr_inputfile
 #if the input file is in csv or tsv format run conversion scripts into fasta
